e064995c1d032bd09c5db80b524b435d13d33950
Alik243 <94523877+Alik243@users.noreply.github.com>
2021-11-18 20:56:36 +0600

    Add files via upload

---
+++ test123/test123/Form1.Designer.cs
@@ -0,0 +1,193 @@
+﻿
+namespace test123
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnGo = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.btnSelect = new System.Windows.Forms.Button();
+            this.pb1 = new System.Windows.Forms.PictureBox();
+            this.rb1 = new System.Windows.Forms.RadioButton();
+            this.rb2 = new System.Windows.Forms.RadioButton();
+            this.tbSelect = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.tbKey = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pb1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnGo
+            // 
+            this.btnGo.Location = new System.Drawing.Point(77, 207);
+            this.btnGo.Name = "btnGo";
+            this.btnGo.Size = new System.Drawing.Size(173, 37);
+            this.btnGo.TabIndex = 0;
+            this.btnGo.Text = "Go";
+            this.btnGo.UseVisualStyleBackColor = true;
+            this.btnGo.Click += new System.EventHandler(this.btnGo_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(23, 29);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(34, 17);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "File";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(23, 162);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 17);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Key";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.ForeColor = System.Drawing.SystemColors.MenuHighlight;
+            this.label3.Location = new System.Drawing.Point(522, 227);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(122, 17);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "made by GorsTen";
+            // 
+            // btnSelect
+            // 
+            this.btnSelect.Location = new System.Drawing.Point(77, 69);
+            this.btnSelect.Name = "btnSelect";
+            this.btnSelect.Size = new System.Drawing.Size(85, 23);
+            this.btnSelect.TabIndex = 7;
+            this.btnSelect.Text = "Select";
+            this.btnSelect.UseVisualStyleBackColor = true;
+            this.btnSelect.Click += new System.EventHandler(this.btnSelect_Click);
+            // 
+            // pb1
+            // 
+            this.pb1.Location = new System.Drawing.Point(426, 29);
+            this.pb1.Name = "pb1";
+            this.pb1.Size = new System.Drawing.Size(218, 155);
+            this.pb1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pb1.TabIndex = 8;
+            this.pb1.TabStop = false;
+            // 
+            // rb1
+            // 
+            this.rb1.AutoSize = true;
+            this.rb1.Location = new System.Drawing.Point(77, 123);
+            this.rb1.Name = "rb1";
+            this.rb1.Size = new System.Drawing.Size(77, 21);
+            this.rb1.TabIndex = 9;
+            this.rb1.TabStop = true;
+            this.rb1.Text = "Encrypt";
+            this.rb1.UseVisualStyleBackColor = true;
+            this.rb1.CheckedChanged += new System.EventHandler(this.rb1_CheckedChanged);
+            // 
+            // rb2
+            // 
+            this.rb2.AutoSize = true;
+            this.rb2.Location = new System.Drawing.Point(172, 123);
+            this.rb2.Name = "rb2";
+            this.rb2.Size = new System.Drawing.Size(78, 21);
+            this.rb2.TabIndex = 10;
+            this.rb2.TabStop = true;
+            this.rb2.Text = "Decrypt";
+            this.rb2.UseVisualStyleBackColor = true;
+            this.rb2.CheckedChanged += new System.EventHandler(this.rb2_CheckedChanged);
+            // 
+            // tbSelect
+            // 
+            this.tbSelect.Location = new System.Drawing.Point(77, 29);
+            this.tbSelect.Name = "tbSelect";
+            this.tbSelect.Size = new System.Drawing.Size(304, 22);
+            this.tbSelect.TabIndex = 11;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label4.Location = new System.Drawing.Point(23, 123);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(28, 17);
+            this.label4.TabIndex = 12;
+            this.label4.Text = "Do";
+            // 
+            // tbKey
+            // 
+            this.tbKey.Location = new System.Drawing.Point(77, 162);
+            this.tbKey.Name = "tbKey";
+            this.tbKey.Size = new System.Drawing.Size(304, 22);
+            this.tbKey.TabIndex = 13;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(676, 273);
+            this.Controls.Add(this.tbKey);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.tbSelect);
+            this.Controls.Add(this.rb2);
+            this.Controls.Add(this.rb1);
+            this.Controls.Add(this.pb1);
+            this.Controls.Add(this.btnSelect);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnGo);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pb1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnGo;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button btnSelect;
+        private System.Windows.Forms.PictureBox pb1;
+        private System.Windows.Forms.RadioButton rb1;
+        private System.Windows.Forms.RadioButton rb2;
+        private System.Windows.Forms.TextBox tbSelect;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox tbKey;
+    }
+}
+
---
+++ test123/test123/Form1.cs
@@ -0,0 +1,187 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace test123
+{
+    public partial class Form1 : Form
+    {
+        byte[] abc;
+        byte[,] table;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSelect_Click(object sender, EventArgs e)
+        {
+            // Открываем файл
+            OpenFileDialog openFile = new OpenFileDialog();
+            openFile.Multiselect = false;
+            openFile.Filter = "Image files(*.WAV;*.JPG;*.MP3)|*.WAV;*.JPG;*.MP3|All Files(*.*)|*.*";
+
+            if (openFile.ShowDialog() == DialogResult.OK)
+            {
+                try
+                {
+                    pb1.Image = new Bitmap(openFile.FileName);
+                    tbSelect.Text = openFile.FileName;
+                }
+                catch (Exception)
+                {
+                    tbSelect.Text = openFile.FileName;
+                    MessageBox.Show("Ошибка открытия картинки", " ", MessageBoxButtons.OK);
+                }
+            }
+        }
+
+        // Выбрать что делать
+        private void rb1_CheckedChanged(object sender, EventArgs e)
+        {
+            if (rb1.Checked)
+            {
+                rb2.Checked = false;
+            }
+        }
+        private void rb2_CheckedChanged(object sender, EventArgs e)
+        {
+            if (rb2.Checked)
+            {
+                rb1.Checked = false;
+            }
+        }
+
+        // Начальные значения при запуске
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            rb1.Checked = true;
+
+            abc = new byte[256];
+            for (int i = 0; i < 256; i++)
+            {
+                abc[i] = Convert.ToByte(i);
+            }
+
+            table = new byte[256, 256];
+            for (int i = 0; i < 256; i++)
+            {
+                for (int j = 0; j < 256; j++)
+                {
+                    table[i, j] = abc[(i + j) % 256];
+                }
+            }
+        }
+
+        // Начало шифрования
+        private void btnGo_Click(object sender, EventArgs e)
+        {
+            if (!File.Exists(tbSelect.Text))
+            {
+                MessageBox.Show("Файл не существует.");
+                return;
+            }
+            if (String.IsNullOrEmpty(tbKey.Text))
+            {
+                MessageBox.Show("Введите ключ для шифрования");
+                return;
+            }
+
+
+            try
+            {
+                byte[] fileContent = File.ReadAllBytes(tbSelect.Text);
+                byte[] passwortTmp = Encoding.ASCII.GetBytes(tbKey.Text);
+                byte[] keys = new byte[fileContent.Length];
+                for (int i = 0; i < fileContent.Length; i++)
+                {
+                    keys[i] = passwortTmp[i % passwortTmp.Length];
+                }
+
+                byte[] result = new byte[fileContent.Length];
+
+                //Encrypt
+
+                if (rb1.Checked)
+                {
+                    for (int i = 0; i < fileContent.Length; i++)
+                    {
+                        byte value = fileContent[i];
+                        byte key = keys[i];
+                        int valueIndex = -1, keyIndex = -1;
+                        for (int j = 0; j < 256; j++)
+                        {
+                            if (abc[j] == value)
+                            {
+                                valueIndex = j;
+                                break;
+                            }
+                        }
+                        for (int j = 0; j < 256; j++)
+                        {
+                            if (abc[j] == key)
+                            {
+                                keyIndex = j;
+                                break;
+                            }
+                        }
+                        result[i] = table[keyIndex, valueIndex];
+                    }
+
+                }
+
+                // Decrypt
+                else
+                {
+                    for (int i = 0; i < fileContent.Length; i++)
+                    {
+                        byte value = fileContent[i];
+                        byte key = keys[i];
+                        int valueIndex = -1, keyIndex = -1;
+                        
+                        for (int j = 0; j < 256; j++)
+                        {
+                            if (abc[j] == key)
+                            {
+                                keyIndex = j;
+                                break;
+                            }
+                        }
+                        for (int j = 0; j < 256; j++)
+                        {
+                            if (table[keyIndex,j] == value)
+                            {
+                                valueIndex = j;
+                                break;
+                            }
+                        }
+                        result[i] = abc[valueIndex];
+                    }
+                }
+
+                //save result
+                String fileExt = Path.GetExtension(tbSelect.Text);
+                SaveFileDialog sd = new SaveFileDialog();
+                sd.Filter = "Files(*" + fileExt + ") | *" + fileExt;
+                if (sd.ShowDialog() == DialogResult.OK)
+                {
+                    File.WriteAllBytes(sd.FileName, result);
+                }
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Файл занят");
+                return;
+            }
+
+
+        }
+    }
+}
